b9d9dc20a32833210234468174da090e22ed782b
NikolayPutyata <nikolayputyata@gmail.com>
2024-09-06 20:06:31 +0300

    upd

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,24 @@ const Hero = () => {
     <section className={s.hero}>
       <div className={s.container}>
         <div className={s.profile}>
-          <img src="/src/img/планшет.jpg" alt="Mykola Putyata" />
+          <picture>
+            <source
+              srcSet="/src/components/Hero/img/планшет.jpg"
+              media="(min-width: 1025px)"
+            />
+            <source
+              srcSet="/src/components/Hero/img/планшет.jpg"
+              media="(min-width: 768px) and (max-width: 1024px)"
+            />
+            <source
+              srcSet="/src/components/Hero/img/телефон.jpg"
+              media="(max-width: 767px)"
+            />
+            <img
+              src="/src/components/Hero/img/планшет.jpg"
+              alt="Mykola Putyata"
+            />
+          </picture>
           <div className={s.divider}></div>
           <div className={s.contacts}>
             <p>FULLSTACK DEVELOPER</p>
